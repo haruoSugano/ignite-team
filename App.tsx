14f89b40da457c4f2caf0a23c6551b4d2f52cf10
haruoSugano <haruosugano@icloud.com>
2023-04-02 21:30:48 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'react-native'; 
+import { ThemeProvider } from "styled-components";
+import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroups';
+import { Players } from '@screens/Players';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
+import theme from '@theme/index';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <Players /> : <Loading />}
+    </ThemeProvider>
+  );
+}
